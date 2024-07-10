9d4703ed4a063057c5252cfbe386d3f77bf49149
Srisagar2005 <166827097+Srisagar2005@users.noreply.github.com>
2024-07-10 12:10:56 +0530

    Add files via upload

---
+++ f.java
@@ -0,0 +1,13 @@
+public class f{
+	public static void main(String[] args){
+		int a=20, b=50;
+		if (a==b)
+		System.out.println("both are equal");
+		else if (a>b)
+		System.out.println(a+" is greater than"+ b);
+		else 
+		System.out.println(b+" is greater than "+a);
+		
+	}
+
+}
